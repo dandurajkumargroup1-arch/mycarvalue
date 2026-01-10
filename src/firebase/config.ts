9baa249cfededf4bdb3f69b0acfaedf70fbfeb6a
dandurajkumarappscgroup1 <dandurajkumargroup1@gmail.com>
2026-01-10 07:07:09 +0000

    check the unwanted api keys

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-675497400-6064f",
   "appId": "1:171392327548:web:81f992000b6f2632044ef9",
-  "apiKey": "AIzaSyCRvQe07bSBqIXXaXxDtIIn2ETCqMRZBaY",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-675497400-6064f.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "171392327548"
