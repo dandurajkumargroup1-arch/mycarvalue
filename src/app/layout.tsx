5ffe955c285023fc4b282b4394cf5cafca5c07f4
dandurajkumarappscgroup1 <dandurajkumargroup1@gmail.com>
2026-01-15 04:37:38 +0000

    see these

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,12 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
