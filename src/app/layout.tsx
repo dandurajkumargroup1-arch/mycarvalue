b105a8acea8b1e632f6c087be267e22f2fe5ded7
dandurajkumarappscgroup1 <dandurajkumargroup1@gmail.com>
2026-01-15 03:25:58 +0000

    just we have uploaded new favications can u please updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export const metadata: Metadata = {
   description: "The smart way to value your car in India.",
   icons: {
     icon: [
+      { url: '/favicon.ico', sizes: 'any', type: 'image/x-icon' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
